87790e400c22fc218ac64aab078524adb606ab1e
Baz <84858527+fcbmaj@users.noreply.github.com>
2021-06-09 15:50:42 -0400

    amended to simple file upload

--- src/index.ts
+++ src/index.ts
@@ -21,14 +21,11 @@ metadata = {
 ondescribe = async function ({ configuration }): Promise<void> {
   postSchema({
     objects: {
-      getid: {
+      "getid": {
         displayName: "Get ID",
         description: "Get Transaction ID",
         properties: {
-          transientDocumentId: {
-            displayName: "transient Document Id",
-            type: "string",
-          },
+          "transientDocumentId": {displayName: "transient Document Id", type: "string"},
         },
         methods: {
           "getDocs": {
@@ -42,67 +39,48 @@ ondescribe = async function ({ configuration }): Promise<void> {
   });
 };
 
-onexecute = async function ({objectName, methodName, parameters, properties, configuration, schema}): Promise<void> {
+onexecute = function ({objectName, methodName, parameters, properties, configuration }) {
   switch (objectName) {
     case "getid":
-      await onexecuteGetId(methodName, parameters, properties, configuration);
+      onexecuteGetId(methodName, parameters, properties, configuration);
       break;
     default:
       throw new Error("The object " + objectName + " is not supported.");
   }
 };
 
-async function onexecuteGetId(methodName: string, parameters: SingleRecord, properties: SingleRecord, configuration: SingleRecord): Promise<void> {
+function onexecuteGetId(methodName, parameters, properties, configuration) {
   switch (methodName) {
     case "getDocs":
-      await onexecuteTransientDocGet(parameters, configuration);
+      onexecuteTransientDocGet(parameters, properties, configuration);
       break;
     default:
       throw new Error("The method " + methodName + " is not supported.");
   }
 }
 
-function onexecuteTransientDocGet(parameters: SingleRecord, configuration: SingleRecord): Promise<void> {
-  return new Promise<void>((resolve, reject) => {
+function onexecuteTransientDocGet(parameters, properties, configuration) {
+  var urlValue = configuration["ServiceURL"];
+  var urlToken = configuration["AccessToken"];
+  var xhr = new XMLHttpRequest();
 
-    var xhr = new XMLHttpRequest();
-    var urlValue = configuration["ServiceURL"];
-    var urlToken = configuration["AccessToken"];
+  var data = new FormData();
+  data.append("File", "sign this doc please");
+  data.append("File-Name", "please_sign.docx");
 
-    var data = new FormData();
-    data.append("File", "sign this doc please");
-    data.append("File-Name", "please_sign.docx");
-
-    var xhr = new XMLHttpRequest();
-
-    xhr.onreadystatechange = function () {
-      try {
-        console.log('1st ready state ' + xhr.readyState);
-        if (xhr.readyState !== 4) return;
-        if (xhr.status !== 200)
-          throw new Error("Failed with status " + xhr.status + "Details: " + xhr.responseText);
-
-        console.log('2nd done')
-
-        var obj = JSON.parse(xhr.responseText);
-        console.log('3rd response text' + xhr.responseText)
-        postResult({
-          transientDocumentId: obj.transientDocumentId, 
-        });
-        resolve();
-      } catch (e) {
-        reject(e);
-      }
-    };
-
-    xhr.open("POST", urlValue + "/transientDocuments");
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.setRequestHeader("x-api-user", "email:nick.williams@ca.fctg.travel");
-    xhr.setRequestHeader("Authorization", "Bearer " + urlToken);
-
-    xhr.send(data);
-    console.log("data being sent " + JSON.stringify(data));
-  });
+  xhr.onreadystatechange = function () {
+    console.log('1st ready state ' + xhr.readyState);
+    if (xhr.readyState !== 4) return;
+    if (xhr.status !== 200) throw new Error("Failed with status " + xhr.status + ". Details: " + xhr.responseText);
+    console.log('2nd response text' + xhr.responseText)
+    postResult({ "transientDocumentId": "transientDocumentId"});
+  };
+  xhr.open("POST", urlValue + "/transientDocuments");
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  xhr.setRequestHeader("x-api-user", "email:nick.williams@ca.fctg.travel");
+  xhr.setRequestHeader("Authorization", "Bearer " + urlToken);
+  xhr.send(data);
+  console.log("data being sent " + JSON.stringify(data));
 }
 
 
