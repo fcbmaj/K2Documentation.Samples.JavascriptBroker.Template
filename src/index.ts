b9a702cc7458c325c9fe837048064b6810ed31bd
Baz <84858527+fcbmaj@users.noreply.github.com>
2021-06-09 19:05:37 -0400

    added promises

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ metadata = {
       }
   }
 };
-ondescribe = function () {
+ondescribe = async function ({configuration}): Promise<void> {
   postSchema({
     objects: {
       "getid": {
@@ -40,68 +40,90 @@ ondescribe = function () {
     },
   });
 };
-onexecute = function ({objectName, methodName, parameters, properties, configuration }) {
+onexecute = async function ({objectName, methodName, parameters, properties, configuration}): Promise<void> {
   switch (objectName) {
-    case "getid":
-      executeXHRTest(methodName, parameters, properties, configuration);
-      break;
-    default:
-      throw new Error("The object " + objectName + " is not supported.");
+      case "getid": await executeXHRTest(methodName, parameters, properties, configuration); break;
+      default: throw new Error("The object " + objectName + " is not supported.");
   }
-};
-function executeXHRTest(methodName, parameters, properties, configuration) {
+}
+async function executeXHRTest(methodName: string, parameters: SingleRecord, properties: SingleRecord, configuration: SingleRecord): Promise<void> {
   switch (methodName) {
-    case "getDocs":
-      executeGetTransDocId(parameters, properties, configuration);
-      break;
-    default:
-      throw new Error("The method " + methodName + " is not supported.");
+      case "getDocs": await executeGetTransDocId(parameters, properties, configuration); break;
+      default: throw new Error("The method " + methodName + " is not supported.");
   }
 }
-function executeGetTransDocId(parameters, properties, configuration) {
-  var urlValue = configuration["ServiceURL"];
-  var urlToken = configuration["AccessToken"];
+// function executeGetTransDocId(parameters, properties, configuration) {
+//   var urlValue = configuration["ServiceURL"];
+//   var urlToken = configuration["AccessToken"];
 
-  var form = new FormData();
-  form.append("File", "sign this doc please");
-  form.append("File-Name", "please_sign.docx");
+//   var form = new FormData();
+//   form.append("File", "sign this doc please");
+//   form.append("File-Name", "please_sign.docx");
 
-  var xhr = new XMLHttpRequest();
-  // xhr.withCredentials = true;
+//   var xhr = new XMLHttpRequest();
+//   // xhr.withCredentials = true;
   
-  xhr.onreadystatechange = function () {
+//   xhr.onreadystatechange = function () {
 
-    if (xhr.readyState !== 4) {
-      return;
-    }
-    if (xhr.readyState === 4) {
-      console.log('readystate == 4 ' + xhr.responseText)
-      var obj = JSON.parse(xhr.responseText);
-      postResult({
-        "transDocId" : obj.transientDocumentId
-      });
-    }
-    if (xhr.status !== 200)
-      throw new Error("Failed with status " + xhr.status + ". Details: " + xhr.responseText);
-    };
-  // xhr.onreadystatechange = function () {
-  //   if(this.readyState === 4) {
-  //     console.log(this.responseText);
-  //     postResult({ "transDocId" :"transientDocumentId" + JSON.stringify(xhr.response)});
-  //   }
-  // };
+//     if (xhr.readyState !== 4) {
+//       return;
+//     }
+//     if (xhr.readyState === 4) {
+//       console.log('readystate == 4 ' + xhr.responseText)
+//       var obj = JSON.parse(xhr.responseText);
+//       postResult({
+//         "transDocId" : obj.transientDocumentId
+//       });
+//     }
+//     if (xhr.status !== 200)
+//       throw new Error("Failed with status " + xhr.status + ". Details: " + xhr.responseText);
+//     };
+//   // xhr.onreadystatechange = function () {
+//   //   if(this.readyState === 4) {
+//   //     console.log(this.responseText);
+//   //     postResult({ "transDocId" :"transientDocumentId" + JSON.stringify(xhr.response)});
+//   //   }
+//   // };
 
-  // xhr.open("POST", urlValue + "/transientDocuments");
-  // xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-  // xhr.setRequestHeader("x-api-user", "email:nick.williams@ca.fctg.travel");
-  // xhr.setRequestHeader("Authorization", "Bearer " + urlToken);
+//   // xhr.open("POST", urlValue + "/transientDocuments");
+//   // xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+//   // xhr.setRequestHeader("x-api-user", "email:nick.williams@ca.fctg.travel");
+//   // xhr.setRequestHeader("Authorization", "Bearer " + urlToken);
 
-  xhr.open("POST", "https://api.na2.adobesign.com/api/rest/v6/transientDocuments");
-  // xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-  xhr.setRequestHeader("x-api-user", "email:nick.williams@ca.fctg.travel");
-  xhr.setRequestHeader("Authorization", "Bearer 3AAABLblqZhAsz7fkOci1ND7WDd20jYyUz2iHXweewyBfHX9jB46rtcAVKjL89-ty8o7dqbFLDVje0C5AF5vG_OC88kQNkjfL");
+//   xhr.open("POST", "https://api.na2.adobesign.com/api/rest/v6/transientDocuments");
+//   // xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+//   xhr.setRequestHeader("x-api-user", "email:nick.williams@ca.fctg.travel");
+//   xhr.setRequestHeader("Authorization", "Bearer 3AAABLblqZhAsz7fkOci1ND7WDd20jYyUz2iHXweewyBfHX9jB46rtcAVKjL89-ty8o7dqbFLDVje0C5AF5vG_OC88kQNkjfL");
 
-  xhr.send(form);
+//   xhr.send(form);
+// }
+
+function executeGetTransDocId(parameters: SingleRecord, properties: SingleRecord, configuration: SingleRecord): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+        // var urlValue = configuration["ServiceURL"];
+        var form = new FormData();
+        form.append("File", "sign this doc please");
+        form.append("File-Name", "please_sign.docx");
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            try {
+                if (xhr.readyState !== 4) return;
+                if (xhr.status !== 200) throw new Error("Failed with status " + xhr.status);
+
+                var obj = JSON.parse(xhr.responseText);
+                postResult({
+                    "transDocId": obj.transientDocumentId
+                  });
+                resolve();
+            } catch (error) {
+                reject(error);
+            }
+
+        }
+
+        xhr.open("POST", "https://api.na2.adobesign.com/api/rest/v6/transientDocuments");
+        xhr.setRequestHeader("x-api-user", "email:nick.williams@ca.fctg.travel");
+        xhr.setRequestHeader("Authorization", "Bearer 3AAABLblqZhAsz7fkOci1ND7WDd20jYyUz2iHXweewyBfHX9jB46rtcAVKjL89-ty8o7dqbFLDVje0C5AF5vG_OC88kQNkjfL");
+        xhr.send();
+    });
 }
-
-
