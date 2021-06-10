a9fce03c50a04df3f8e33b06e9e439ee8b33669f
Baz <84858527+fcbmaj@users.noreply.github.com>
2021-06-10 15:37:39 -0400

    removed getfile back to content

--- src/index.ts
+++ src/index.ts
@@ -82,8 +82,8 @@ function executeUploadFileMethod(parameters, properties, configuration) {
   console.log("filename: " + properties["file1"].filename);
 
   // get file content
-  form.append('file', properties["file1"].GetFile);
-  console.log("content: " + properties["file1"].GetFile);
+  form.append('file', properties["file1"].content);
+  console.log("content: " + properties["file1"].content);
 
   var xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function () {
