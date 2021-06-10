acf58890e7ca5ff27b27e2141c4be6db74d2a9e2
Baz <84858527+fcbmaj@users.noreply.github.com>
2021-06-10 11:59:46 -0400

    stringify of attributes

--- src/index.ts
+++ src/index.ts
@@ -53,7 +53,7 @@ function executeUploadFileMethod(parameters, properties) {
 
   var form = new FormData();
   // form.append('attributes', JSON.stringify({"name": properties[file1].filename, "parent": {"id": "0"}})); //IMPORTANT
-  form.append("attributes", "{\n  \"name\": \"TestTwo.docx\",\n  \"parent\": {\n    \"id\": \"0\"\n  },\n  \"content_created_at\": \"2012-12-12T10:53:43-08:00\",\n  \"content_modified_at\": \"2012-12-12T10:53:43-08:00\"\n}");
+  form.append("attributes", JSON.stringify({"name": properties[file1].filename, "parent": {"id": "0"},"content_created_at": "2012-12-12T10:53:43-08:00", "content_modified_at": "2012-12-12T10:53:43-08:00"}));
   form.append('file', properties[file1].content);
 
       
