34d12ec2049cfdbfed3b78485feb919292af009a
Baz <84858527+fcbmaj@users.noreply.github.com>
2021-06-10 17:01:58 -0400

    ignored content-type

--- src/index.ts
+++ src/index.ts
@@ -100,7 +100,7 @@ function executeUploadFileMethod(parameters, properties, configuration) {
   };
 
   xhr.open("POST", urlValue + "/transientDocuments");
-  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  //xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
   xhr.setRequestHeader("x-api-user", "email:nick.williams@ca.fctg.travel");
   xhr.setRequestHeader("Authorization", "Bearer " + urlToken);
   xhr.send(form);
